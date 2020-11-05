006687e87e1dd43b71b7cdd9830d91dc2a0e406c
xuwenli0225 <435854367.qq.com>
2020-11-05 18:46:29 +0800

    实现简易VueRouter

--- src/krouter/kvue-router.js
+++ src/krouter/kvue-router.js
@@ -1,46 +1,36 @@
-let _Vue
-
-// 我们自己的router实现
-// 插件定义
+let _Vue;
+// 1.实现VueRouter类
 class VueRouter {
   constructor(options) {
+    // 接受options(routes)
     this.$options = options;
-
-    // 创建一个保存url变量
-    // current必须是一个响应式的数据
-    // 好处是未来router-view使用current时就产生依赖关系
-    // current发生变化，router-view的render会重新执行
-    const initial = window.location.hash.slice(1) || "/";
-    _Vue.util.defineReactive(this, 'current', initial)
-
-    // 监视url的变化
+    // 创建一个响应式变量current,并初始化
+    const initial = window.location.hash.slice(1) | "/";
+    _Vue.util.defineReactive(this, "current", initial);
+    // 监视url变化,url变化就更新current
     window.addEventListener("hashchange", this.onHashChange.bind(this));
     window.addEventListener("load", this.onHashChange.bind(this));
   }
-
+  // url变化的回调
   onHashChange() {
     this.current = window.location.hash.slice(1);
   }
 }
 
-// 外面Vue.use会调用install
-VueRouter.install = function(Vue) {
-  // 0。保存一下Vue
-  _Vue = Vue
-  
-  // 1.挂载一下$router
+// 2.实现一个install方法，Vue.use()时执行
+VueRouter.install = function (Vue) {
+  _Vue = Vue;
+  // 通过混入在Vue原型上添加$router(通过new VueRouter实例化的路由对象)
   Vue.mixin({
     beforeCreate() {
-      // 获取router实例
+      // 存在说明是根实例
       if (this.$options.router) {
-        // 如果存在，说明这是根实例
         Vue.prototype.$router = this.$options.router;
       }
     },
   });
 
-  // 2.实现两个全局组件
-  // <router-link to="/xxx">xxx<router-link>
+  // 实现一个router-link标签
   Vue.component("router-link", {
     props: {
       to: {
@@ -49,27 +39,21 @@ VueRouter.install = function(Vue) {
       },
     },
     render(h) {
-      // jsx写法
-      // return <a href={'#'+this.to}>{this.$slots.default}</a>
-      // <a href="#/xxx">ooo</a>
-      // 参数1-标签类型
-      // 参数2-元素属性
-      // 参数3-孩子元素
       return h("a", { attrs: { href: "#" + this.to } }, this.$slots.default);
     },
   });
+
+  // 实现一个router-view标签,根据路由规则返回一个组件
   Vue.component("router-view", {
     render(h) {
-      // 1.获取路由器实例
-      let component = null
-      const route = this.$router.$options.routes.find(
+      let component = null;
+      // $router是用混入在vue原型上挂载的
+      let route = this.$router.$options.routes.find(
         (route) => route.path === this.$router.current
       );
       if (route) {
-        component = route.component
+        component = route.component;
       }
-      // 获取当前url中的hash #/about
-      // 从路由表中获取对应的组件
       return h(component);
     },
   });
