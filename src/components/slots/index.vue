da2ccb5ae5d005653805893d15c413d401bb753e
xuwenli0225 <435854367.qq.com>
2020-11-05 15:04:10 +0800

    init

---
+++ src/components/slots/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>插槽</h2>
+    <!-- 插槽 -->
+    <Layout>
+      <!-- 具名插槽 -->
+      <template v-slot:header>Hello Chino</template>
+      <!-- 匿名插槽 -->
+      <template>content...</template>
+      <!-- 作用域插槽 -->
+      <template v-slot:footer="{ fc }">{{ fc }}</template>
+    </Layout>
+  </div>
+</template>
+
+<script>
+import Layout from "@/components/slots/Layout.vue";
+
+export default {
+  components: {
+    Layout,
+  },
+};
+</script>
+
+<style scoped></style>
