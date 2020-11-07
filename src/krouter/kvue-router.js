380b24a69c15005f2b24ae91d108efda8973f044
xuwenli <xuwenli0225.com>
2020-11-07 22:42:17 +0800

    实现嵌套路由

--- src/krouter/kvue-router.js
+++ src/krouter/kvue-router.js
@@ -1,62 +1,98 @@
-let _Vue;
-// 1.实现VueRouter类
+let _Vue
+
 class VueRouter {
   constructor(options) {
-    // 接受options(routes)
-    this.$options = options;
-    // 创建一个响应式变量current,并初始化
-    const initial = window.location.hash.slice(1) | "/";
-    _Vue.util.defineReactive(this, "current", initial);
-    // 监视url变化,url变化就更新current
-    window.addEventListener("hashchange", this.onHashChange.bind(this));
-    window.addEventListener("load", this.onHashChange.bind(this));
+    this.$options = options
+    // const initial = window.location.hash.slice(1) | '/'
+    // _Vue.util.defineReactive(this, "current", initial)
+
+    this.current = window.location.hash.slice(1) || '/'
+    _Vue.util.defineReactive(this, 'matched', [])
+    this.match()
+
+    window.addEventListener('hashchange', this.onHashChange.bind(this))
+    window.addEventListener('load', this.onHashChange.bind(this))
+
   }
-  // url变化的回调
-  onHashChange() {
-    this.current = window.location.hash.slice(1);
+  onHashChange () {
+    this.current = window.location.hash.slice(1)
+    this.matched = []
+    this.match()
+  }
+
+  match (routes) {
+    routes = routes || this.$options.routes
+    for (const route of routes) {
+      if (route.path === '/' && this.current === '/') {
+        this.matched.push(route)
+        return
+      }
+      if (route.path !== '/' && this.current.indexOf(route.path) != -1) {
+        this.matched.push(route)
+        if (route.children) {
+          this.match(route.children)
+        }
+        return
+      }
+    }
   }
 }
 
-// 2.实现一个install方法，Vue.use()时执行
-VueRouter.install = function (Vue) {
-  _Vue = Vue;
-  // 通过混入在Vue原型上添加$router(通过new VueRouter实例化的路由对象)
-  Vue.mixin({
-    beforeCreate() {
-      // 存在说明是根实例
-      if (this.$options.router) {
-        Vue.prototype.$router = this.$options.router;
-      }
-    },
-  });
 
-  // 实现一个router-link标签
+VueRouter.install = function (Vue) {
+  _Vue = Vue
+  Vue.mixin({
+    beforeCreate () {
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
+    }
+  })
   Vue.component("router-link", {
     props: {
       to: {
         type: String,
-        required: true,
+        required: true
       },
     },
-    render(h) {
-      return h("a", { attrs: { href: "#" + this.to } }, this.$slots.default);
-    },
-  });
+    render (h) {
+      return h("a", { attrs: { href: "#" + this.to } }, this.$slots.default)
+    }
+  })
 
-  // 实现一个router-view标签,根据路由规则返回一个组件
   Vue.component("router-view", {
-    render(h) {
-      let component = null;
-      // $router是用混入在vue原型上挂载的
-      let route = this.$router.$options.routes.find(
-        (route) => route.path === this.$router.current
-      );
-      if (route) {
-        component = route.component;
-      }
-      return h(component);
-    },
-  });
-};
+    render (h) {
+      console.log(this)
+      this.$vnode.data.routerView = true
 
-export default VueRouter;
+      let depth = 0
+      let parent = this.$parent
+      while (parent) {
+        const vnodeData = parent.$vnode && parent.$vnode.data
+        if (vnodeData) {
+          if (vnodeData.routerView) {
+            depth++
+          }
+        }
+
+        parent = parent.$parent
+      }
+
+
+
+      let component = null
+      // const route = this.$router.$options.routes.find((route) =>
+      //   route.path === this.$router.current
+      // )
+      const route = this.$router.matched[depth]
+      if (route) {
+        component = route.component
+      }
+      return h(component)
+    }
+  })
+
+
+}
+
+export default VueRouter
